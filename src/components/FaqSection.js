6227902cfe1c7e7344a62cb434bac86fc0bfedad
Ernestas Milieska <ernestasmilieska@gmail.com>
2022-06-15 21:53:15 +0300

    FAQ section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const FaqSection = () => {
+    return(
+        <div className="faq">
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="question">
+                <h4>How Do I Start</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci culpa 
+                    neque ut expedita asperiores libero, odio et tenetur aperiam tempora.
+                </p>
+                <div className="question">
+                <h4>Daily Schedule</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci culpa 
+                    neque ut expedita asperiores libero, odio et tenetur aperiam tempora.
+                </p>
+            </div>
+            <div className="question">
+                <h4>Diferrent Payment Methods</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci culpa 
+                    neque ut expedita asperiores libero, odio et tenetur aperiam tempora.
+                </p>
+            </div>
+            <div className="question">
+                <h4>What Products Do you Offer</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci culpa 
+                    neque ut expedita asperiores libero, odio et tenetur aperiam tempora.
+                </p>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default FaqSection;

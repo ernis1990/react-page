ad21358b195398a6254928374c093ff50e3f7bd4
Ernestas Milieska <ernestasmilieska@gmail.com>
2022-06-27 21:46:18 +0300

    Styled "Faq section"

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,39 +1,81 @@
 import React from "react";
+import{ About, Description, Image, Hide} from "../styles";
+import styled from "styled-components";
 
 const FaqSection = () => {
     return(
-        <div className="faq">
+        <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
             <div className="question">
                 <h4>How Do I Start</h4>
-                <p>Lorem ipsum dolor sit amet.</p>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci culpa 
-                    neque ut expedita asperiores libero, odio et tenetur aperiam tempora.
-                </p>
-                <div className="question">
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci culpa 
+                        neque ut expedita asperiores libero, odio et tenetur aperiam tempora.
+                    </p>
+                </div>
+                <div className="faq-line"></div>
+            </div>
+            <div className="question">
                 <h4>Daily Schedule</h4>
-                <p>Lorem ipsum dolor sit amet.</p>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci culpa 
-                    neque ut expedita asperiores libero, odio et tenetur aperiam tempora.
-                </p>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci culpa 
+                        neque ut expedita asperiores libero, odio et tenetur aperiam tempora.
+                    </p>
+                </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Diferrent Payment Methods</h4>
-                <p>Lorem ipsum dolor sit amet.</p>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci culpa 
-                    neque ut expedita asperiores libero, odio et tenetur aperiam tempora.
-                </p>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci culpa 
+                        neque ut expedita asperiores libero, odio et tenetur aperiam tempora.
+                    </p>
+                </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>What Products Do you Offer</h4>
-                <p>Lorem ipsum dolor sit amet.</p>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci culpa 
-                    neque ut expedita asperiores libero, odio et tenetur aperiam tempora.
-                </p>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci culpa 
+                        neque ut expedita asperiores libero, odio et tenetur aperiam tempora.
+                    </p>
+                </div>
+                <div className="faq-line"></div>
             </div>
-            </div>
-        </div>
+
+        </Faq>
     )
 }
+
+const Faq = styled(About)`
+    display: block;
+    span{
+        display: block;
+    }
+    h2{
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .faq-line{
+        background: #cccccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+    }
+    .question{
+        padding: 3rem 0rem;
+        cursor: pointer;
+    }
+    .answer{
+        padding: 2rem 0rem;
+        p{
+            padding: 1rem 0rem;
+        }
+    }
+`;
 
 export default FaqSection;

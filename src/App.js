1d88116dc083297bdd05a3aea18d1c5dbfc737bf
Ernestas Milieska <ernestasmilieska@gmail.com>
2022-06-27 20:24:14 +0300

    Styling About Section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import AboutUs from "./pages/AboutUs";
+import GlobalStyle from "./components/GlobalStyle";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle/>
       <AboutUs/>
+      <h1>labaas</h1>
     </div>
   );
 }
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,10 +1,14 @@
 import React from "react";
 import home1 from "../img/home1.png"
 
+import styled from "styled-components";
+
+
+
 const AboutSection = () => {
     return(
-        <div>
-            <div className="description">
+        <About>
+            <Description>
                 <div className="title">
                     <div className="hide">
                         <h2>We work to make</h2>
@@ -18,12 +22,36 @@ const AboutSection = () => {
                 </div>
                 <p>Contact us for any photography or videography ideas that you have. We have profrssionals with amazing skills.</p>
                 <button>Contact Us</button>
-            </div>
-            <div className="image">
+            </Description>
+            <Image>
                 <img src={home1} alt="guy with a camera" />
-            </div>
-        </div>
+            </Image>
+        </About>
     )
 }
 
+const About = styled.div`
+    min-height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: white;
+`;
+const Description = styled.div`
+    flex: 1;
+    padding-right:  5rem;
+    h2{
+        font-weight: lighter;
+    }
+`;
+const Image = styled.div`
+    flex: 1;
+    overflow: hidden;
+    img{
+        width: 100%;
+        height: 80vh;
+        object-fit: cover;
+    }
+`;
 export default AboutSection;

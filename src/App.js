4c1698bccf682f3225ed42545d42486d36ad6e79
Ernestas Milieska <ernestasmilieska@gmail.com>
2022-06-28 19:52:00 +0300

    Add ROUTER

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from "react";
 import AboutUs from "./pages/AboutUs";
 import GlobalStyle from "./components/GlobalStyle";
 import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import {Route, Routes } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
       <Nav/>
-      <AboutUs/>
-      <h1>labaas</h1>
+      <Routes>
+        <Route path='/' element={<AboutUs/>}/>
+        <Route path='/contact' element={<ContactUs/>}/>
+        <Route path='/work' element={<OurWork/>}/>
+      </Routes>
+
+      
     </div>
   );
 }
